fb54124b54aae762c4f79e88922d19d8d2f1b0c2
bhavesh <bhaveshkapoor09@gmail.com>
2024-02-14 12:32:34 +0530

    add comments

--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,20 @@ Route::get('/', [IndexController::class, 'index']);
 
 
 // admin panel routes
-Route::put('login', [IndexController::class, 'index']);
-Route::post('/auth', [IndexController::class, 'check']);
-Route::get('/admins', [IndexController::class, 'index']);
+Route::put('login', [IndexController::class, 'index']);   
+Route::post('/auth', [IndexController::class, 'check']); // verify whether the credentials are valid or not
+Route::get('/admins', [IndexController::class, 'index']);   // redirect to the login page of the login
 
 Route::group(['middleware' => 'auth', 'prefix' => '/admin'], function () {
 
     
-    Route::get('/dashboard', [DashboardController::class, 'index']);
-    Route::get('/change_password', [DashboardController::class, 'change_password']);
-    Route::post('/update_password', [DashboardController::class, 'update_password']);
 
-
+    Route::get('/dashboard', [DashboardController::class, 'index']);   // The  dashboard page
+    Route::get('/change_password', [DashboardController::class, 'change_password']); // change password
+    Route::post('/update_password', [DashboardController::class, 'update_password']); // Update password
     Route::post('/registrations/delete', [RegistrationController::class, 'delete']);
-    Route::resource('registrations', RegistrationController::class);
-    Route::get('registrations/view/{id}', [RegistrationController::class,'view']);
-
-  
+    Route::resource('registrations', RegistrationController::class);   // Registration form controller functions
+    Route::get('registrations/view/{id}', [RegistrationController::class,'view']);   // View of the user dettail
 
 });
 
